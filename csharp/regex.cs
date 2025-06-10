da629f9a6427424df7a5f66fe48f5fdf6c27fecf
tyson0828 <tyson0828@users.noreply.github.com>
2025-06-10 14:20:07 -0700

    Update regex.cs

--- csharp/regex.cs
+++ csharp/regex.cs
@@ -1,1 +1,21 @@
-var pattern = @"[.][\\/]+Modules[\\/]+[^\\/]+[\\/]+[^\\/]+\.mtpl";
+using System;
+using System.Text.RegularExpressions;
+
+string[] lines = {
+    "./Modules/ARR_ABC/ARR_ABC.mtpl",
+    ".\\Modules\\ARR_XXX\\ARR_XXX.mtpl",
+    "./Modules/MyTest/MyFile.mtpl"
+};
+
+var pattern = @"[.][\\/](Modules[\\/][^\\/]+[\\/][^\\/]+\.mtpl)";
+
+foreach (var line in lines)
+{
+    var match = Regex.Match(line, pattern);
+    if (match.Success)
+    {
+        // Extract and normalize to forward slashes
+        string path = match.Groups[1].Value.Replace('\\', '/');
+        Console.WriteLine($"Extracted: {path}");
+    }
+}
